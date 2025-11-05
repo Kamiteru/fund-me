8a60aa0dd35a458e6618108ca9079522a1842b7e
Kamiteru <yokai1377@gmail.com>
2025-11-05 10:55:33 +0000

    first commit

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,62 @@
+//  SPDX-License-Identifier: MIT
+
+// 1. Deploy mock when we are on a local chain
+// 2. Keep track of contract addresses across different networks
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITITAL_PRICE = 2000e8;
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreatAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5147eA642CAEF7BD9c1265AadcA78f997AbB9649
+        });
+        return ethConfig;
+    }
+
+    function getOrCreatAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        // Deploy the mocks
+        // Return the mock address
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITITAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory getAnvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return getAnvilConfig;
+    }
+}
